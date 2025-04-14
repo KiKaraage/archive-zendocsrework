6b1148187d1ec39750c5376d71ca2a8550c5d7de
Jonas List <Jonas.List.1289@gmail.com>
2025-04-14 19:26:08 +0200

    Added basePath for production environment

--- next.config.mjs
+++ next.config.mjs
@@ -2,8 +2,11 @@ import { createMDX } from 'fumadocs-mdx/next';
 
 const withMDX = createMDX();
 
+const isProd = process.env.NODE_ENV === 'production';
+
 /** @type {import('next').NextConfig} */
 const config = {
+  basePath: isProd ? '/zen-docs' : '',
   output: "export",
   reactStrictMode: true,
   images: { unoptimized: true }
