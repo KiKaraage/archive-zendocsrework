14e1c47af81c75e25a8b2bcd34467c2f13e6d9ec
Jonas List <Jonas.List.1289@gmail.com>
2025-04-14 20:58:53 +0200

    Removed basePath condition in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,11 +2,8 @@ import { createMDX } from 'fumadocs-mdx/next';
 
 const withMDX = createMDX();
 
-const isProd = process.env.NODE_ENV === 'production';
-
 /** @type {import('next').NextConfig} */
 const config = {
-  basePath: isProd ? '/zen-docs' : '',
   output: "export",
   reactStrictMode: true,
   images: { unoptimized: true }
